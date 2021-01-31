99ef87c13f57457303677569eddd453e882c37de
ViniciusGiroto <viniciusgiroto@outlook.com>
2021-01-31 20:18:06 -0300

    Fix

--- routes/upload.js
+++ routes/upload.js
@@ -1,32 +1,21 @@
 
 const router = require("express").Router();
-const sqlite3 = require('sqlite3').verbose();
+const knex = require('knex');
 
 router.post('/upload', async (req,res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
-    var database = req.files.vocabulary;
+    const db = knex({
+      client: 'sqlite3',
+      connection: {
+        filename: req.files.vocabulary.tempFilePath
+      }
+    });
 
-    let db = new sqlite3.Database(database.tempFilePath, sqlite3.OPEN_READWRITE, (err) => {
-        if (err) {
-          console.error(err.message);
-        } else {
-          console.log('Connected to the Kindle database.');
-        }
-      });
-    
-    let vocabulary = () => {
-        let query = `SELECT * FROM LOOKUPS`
-        let result = []
-        db.each(query, (err, row) => {
-            result.push(row)
-        }, () => {
-            return result
-        })
-    }
+    const result = await db.select().table('LOOKUPS');
 
-    res.send(vocabulary())
+    res.send(result);
 })
 module.exports = router;
